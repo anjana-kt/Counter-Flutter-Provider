d19f079f4c8af047f59fdf2ced4c033be9ac74df
Anjana <58140600+anjana-kt@users.noreply.github.com>
2022-07-26 23:25:07 +0530

    parse server added

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,90 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:counter/count.dart';
-import 'package:counter/counter.dart';
+import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+import 'dart:convert';
+
+dynamic k = 0;
+dynamic c,count=0;
+void parseInit()async{
+  var ipaddress = "http://192.168.1.10:1337/parse";
+  await Parse().initialize(
+      'myAppId',
+      // 'http://localhost:1337/parse',
+      ipaddress,
+      masterKey: "masterKey",
+      clientKey: "clientKey",
+      debug: true,
+      coreStore: await CoreStoreSharedPrefsImp.getInstance()
+  );
+
+  final ParseResponse response = await Parse().healthCheck();
+  print(response.toString());
+  if (response.success) {
+    print("server health check success");
+    c = await ParseObject('Counter')
+      ..objectId='5oBq0BC3oo';
+      // ..set<int>('count',0);
+    // var response = await c.save();
+    // if (response.success) {
+    //   c = response.results?.first;
+    //   print("Counter initialised with : ${c.toString()}");
+    // }
+    count =  c..get<int>('count');
+    k = count.toString();
+    if(k!=0 && k!=null){
+      print("fetch success $k");
+    }
+    else{
+      print("Failed to initialise");
+    }
+  } else {
+    print('Server health check failed');
+  }
+}
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-  create: (_) => Count(),
-  child: MyApp(),
-)
+      MaterialApp(home: Counter())
   );
 }
 
-class MyApp extends StatelessWidget {
-  
+class Counter extends StatefulWidget {
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  @override
+  void initState() {
+    super.initState();
+    parseInit();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Counter(),
+    return Scaffold(
+      appBar: AppBar(title: const Text('My Counter'),),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add, color: Colors.white,),
+        onPressed: ()async{
+          // count = await c.increment("count", 1);
+          count = c..setIncrement('count', 1);
+          var res =await count.save();
+          print("incremented : ${res.results.first}");
+          // count =  c..get<int>('count');
+          (count != 0)? print("Success, incremented!") : print("Failed to increment");
+          setState(() {
+            k=res.results.first;
+            print(k);
+          });
+        },
+      ),
+      body: Center(
+          child: Text(k.toString(),
+            style: const TextStyle(color: Colors.lightBlueAccent,
+                fontSize: 70),)
+      ),
     );
-
   }
 }
+
